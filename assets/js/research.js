fe87e22750f6991d2d2d53743e48072772f33304
fluna <lunadanielfelipe@gmail.com>
2024-11-01 13:32:27 -0500

    cambios nosotros

--- assets/js/research.js
+++ assets/js/research.js
@@ -4,147 +4,44 @@ const researchTable = document.querySelector(".main");
 
 const research = [
   {
-    title: "A Batch Normalized Inference Network Keeps the KL Vanishing Away",
+    title: "MISION",
     authors:
-      "Qile Zhu, Wei Bi, Xiaojiang Liu, Xiyao Ma, Xiaolin Li and Dapeng Wu",
+      "DWD Solutions",
     conferences:
-      "The 58th Annual Meeting of the Association for Computational Linguistics",
-    researchYr: 2020,
-    citebox: "popup1",
-    image: "assets/images/research-page/inteferenceNetwork.png",
-    citation: {
-      vancouver:
-        "Qile Zhu, Wei Bi, Xiaojiang Liu, Xiyao Ma, Xiaolin Li and Dapeng Wu. A Batch Normalized Inference Network Keeps the KL Vanishing Away. The 58th Annual Meeting of the Association for Computational Linguistics 2020.",
-    },
-    abstract:
-      "This is currently left empty and this can be considered as a dummy data 1",
-    absbox: "absPopup1",
+      "Crear experiencias digitales únicas mediante el desarrollo personalizado de aplicaciones web innovadoras, funcionales y escalables, con el fin de ayudar a nuestros clientes a destacarse en el mundo digital, optimizando sus procesos y alcanzando sus objetivos de negocio con éxito. ",
+    researchYr: 2024,
+    image: "assets/images/research-page/mision.png",
+    
   },
-
   {
-    title: "A Call for More Rigor in Unsupervised Cross-lingual Learning",
+    title: "VISION",
     authors:
-      "Mikel Artetxe, Sebastian Ruder, Dani Yogatama, Gorka Labaka and Eneko Agirre",
+      "DWD Solutions",
     conferences:
-      "The 58th Annual Meeting of the Association for Computational Linguistics",
-    researchYr: 2020,
-    citebox: "popup2",
-    image: "assets/images/research-page/crossLingual.png",
-    citation: {
-      vancouver:
-        "Mikel Artetxe, Sebastian Ruder, Dani Yogatama, Gorka Labaka and Eneko Agirre. A Call for More Rigor in Unsupervised Cross-lingual Learning. The 58th Annual Meeting of the Association for Computational Linguistics 2020.",
-    },
-    abstract:
-      "This is currently left empty and this can be considered as a dummy data 2",
-    absbox: "absPopup2",
+      "Ser una empresa reconocida internacionalmente para el año 2029 por nuestra labor y compromiso en ofrecer soluciones tecnológicas innovadoras que generen un impacto positivo y medible en nuestros clientes ",
+    researchYr: 2024,
+    image: "assets/images/research-page/vision.png",
+    
   },
-
   {
-    title:
-      "A Comprehensive Analysis of Preprocessing for Word Representation Learning in Affective Tasks",
-    authors: "Nastaran Babanejad, Ameeta Agrawal, Aijun An and Manos Papagelis",
+    title: "NUESTROS VALORES",
     conferences:
-      "The 58th Annual Meeting of the Association for Computational Linguistics",
-    researchYr: 2020,
-    citebox: "popup3",
-    image: "assets/images/research-page/wordRepresentation.png",
-    citation: {
-      vancouver:
-        "Nastaran Babanejad, Ameeta Agrawal, Aijun An and Manos Papagelis. A Comprehensive Analysis of Preprocessing for Word Representation Learning in Affective Tasks. The 58th Annual Meeting of the Association for Computational Linguistics 2020.",
-    },
-    abstract:
-      "This is currently left empty and this can be considered as a dummy data 3",
-    absbox: "absPopup3",
-  },
-
-  {
-    title:
-      "A Contextual Hierarchical Attention Network with Adaptive Objective for Dialogue State Tracking",
+    "",
     authors:
-      "Yong Shan, Zekang Li, Jinchao Zhang, Fandong Meng, Yang Feng, Cheng Niu and Jie Zhou",
-    conferences:
-      "The 58th Annual Meeting of the Association for Computational Linguistics",
-    researchYr: 2020,
-    citebox: "popup4",
-    image: "assets/images/research-page/dialogueState.png",
-    citation: {
-      vancouver:
-        "Yong Shan, Zekang Li, Jinchao Zhang, Fandong Meng, Yang Feng, Cheng Niu and Jie Zhou. A Contextual Hierarchical Attention Network with Adaptive Objective for Dialogue State Tracking. The 58th Annual Meeting of the Association for Computational Linguistics 2020.",
-    },
-    abstract:
-      "This is currently left empty and this can be considered as a dummy data 4",
-    absbox: "absPopup4",
+      "DWD Solutions",
+    primer:
+      "Innovación Constante: Creemos en la importancia de estar a la vanguardia tecnológica, buscando continuamente nuevas formas de mejorar y ofrecer soluciones más eficientes y creativas para nuestros clientes",
+    segundo:
+      "Agilidad y Adaptabilidad: Nos destacamos por nuestra capacidad de adaptarnos rápidamente a los cambios en el mercado y las necesidades de nuestros clientes, asegurando que nuestras soluciones sean siempre relevantes y efectivas",
+    tercer:
+      "Integridad y Transparencia: Operamos con honestidad y transparencia en todas nuestras interacciones, construyendo relaciones de confianza con nuestros clientes",
+    cuarto:
+      "Compromiso con el Éxito: Nos comprometemos a ser un socio estratégico para nuestros clientes, apoyándolos en su crecimiento digital y ayudándolos a destacar en el mercado",
+    researchYr: "",
+    image: "assets/images/research-page/valores.webp",
+    
   },
 
-  {
-    title: "Dual Super-Resolution Learning for Semantic Segmentation",
-    authors: "Wang, Li and Li, Dong and Zhu, Yousong and Tian, Lu and Shan, Yi",
-    conferences:
-      "IEEE/CVF Conference on Computer Vision and Pattern Recognition (CVPR)",
-    researchYr: 2020,
-    citebox: "popup5",
-    image: "assets/images/research-page/semanticSegmentation.png",
-    citation: {
-      vancouver:
-        "Wang, Li and Li, Dong and Zhu, Yousong and Tian, Lu and Shan, Yi. Dual Super-Resolution Learning for Semantic Segmentation. IEEE/CVF Conference on Computer Vision and Pattern Recognition (CVPR) 2020.",
-    },
-    abstract:
-      "This is currently left empty and this can be considered as a dummy data 5",
-    absbox: "absPopup5",
-  },
-
-  {
-    title: "Deep Unfolding Network for Image Super-Resolution",
-    authors: "Zhang, Kai and Van Gool, Luc and Timofte, Radu",
-    conferences:
-      "IEEE/CVF Conference on Computer Vision and Pattern Recognition (CVPR)",
-    researchYr: 2020,
-    citebox: "popup6",
-    image: "assets/images/research-page/deepNetwork.png",
-    citation: {
-      vancouver:
-        "Zhang, Kai and Van Gool, Luc and Timofte, Radu. Deep Unfolding Network for Image Super-Resolution. IEEE/CVF Conference on Computer Vision and Pattern Recognition (CVPR) 2020.",
-    },
-    abstract:
-      "This is currently left empty and this can be considered as a dummy data 6",
-    absbox: "absPopup6",
-  },
-
-  {
-    title:
-      "Unsupervised Learning for Intrinsic Image Decomposition From a Single Image",
-    authors: "Liu, Yunfei and Li, Yu and You, Shaodi and Lu, Feng",
-    conferences:
-      "IEEE/CVF Conference on Computer Vision and Pattern Recognition (CVPR)",
-    researchYr: 2020,
-    citebox: "popup7",
-    image: "assets/images/research-page/imageDecomposition.png",
-    citation: {
-      vancouver:
-        "Liu, Yunfei and Li, Yu and You, Shaodi and Lu, Feng. Unsupervised Learning for Intrinsic Image Decomposition From a Single Image. IEEE/CVF Conference on Computer Vision and Pattern Recognition (CVPR) 2020.",
-    },
-    abstract:
-      "This is currently left empty and this can be considered as a dummy data 7",
-    absbox: "absPopup7",
-  },
-  {
-    title:
-      "Forward and Backward Information Retention for Accurate Binary Neural Networks",
-    authors:
-      "Qin, Haotong and Gong, Ruihao and Liu, Xianglong and Shen, Mingzhu and Wei, Ziran and Yu, Fengwei and Song, Jingkuan",
-    conferences:
-      "IEEE/CVF Conference on Computer Vision and Pattern Recognition (CVPR)",
-    researchYr: 2020,
-    citebox: "popup8",
-    image: "assets/images/research-page/neuralNetworks.jpg",
-    citation: {
-      vancouver:
-        "Qin, Haotong and Gong, Ruihao and Liu, Xianglong and Shen, Mingzhu and Wei, Ziran and Yu, Fengwei and Song, Jingkuan. Forward and Backward Information Retention for Accurate Binary Neural Networks. IEEE/CVF Conference on Computer Vision and Pattern Recognition (CVPR) 2020.",
-    },
-    abstract:
-      "This is currently left empty and this can be considered as a dummy data 8",
-    absbox: "absPopup8",
-  },
 ];
 AOS.init();
 const fillData = () => {
@@ -156,50 +53,36 @@ const fillData = () => {
       authors,
       conferences,
       researchYr,
-      citebox,
-      citation,
-      absbox,
-      abstract,
+      primer,
+      segundo,
+      tercer,
+      cuarto
     }) =>
       (output += `
-            <tr data-aos="zoom-in-left"> 
-                <td class="imgCol"><img src="${image}" class="rImg"></td>
-                <td class = "researchTitleName">
-                    <div class="img-div">
-                        <span class="imgResponsive">
-                            <img src="${image}" class="imgRes">
-                        </span>
-                    </div>
-                    <a href="#0" class="paperTitle"> ${title} </a> 
-                    <div class = "authors"> ${authors} </div> 
-                    
-                    <div class="rConferences"> ${conferences} 
-                        <div class="researchY">${researchYr}</div>
-                    </div>
-                    
-                    <!--CITE BUTTON-->
-                    <div class="d-flex" style="margin-right:5%;">
-                        <button class="button button-accent button-small text-right button-abstract " type="button" data-toggle="collapse" data-target="#${absbox}" aria-expanded="false" aria-controls="${absbox}">
-                            ABSTRACT
-                        </button>
-                
-                        <button class="button button-accent button-small text-right button-abstract " type="button" data-toggle="collapse" data-target="#${citebox}" aria-expanded="false" aria-controls="${citebox}">
-                            CITE
-                        </button>
-                    </div>
-                    <div id="${absbox}" class="collapse" aria-labelledby="headingTwo" data-parent=".collapse">
-                        <div class="card-body">
-                            ${abstract}    
-                        </div>
-                    </div>
-                    <div id="${citebox}" class="collapse" aria-labelledby="headingTwo" data-parent=".collapse">
-                        <div class="card-body">
-                            ${citation.vancouver}    
-                        </div>
-                    </div>
-                </td>
-            </tr>`)
-  );
+        <tr data-aos="zoom-in-left"> 
+            <td class="imgCol"><img src="${image}" class="rImg"></td>
+            <td class="researchTitleName">
+                <div class="img-div">
+                    <span class="imgResponsive">
+                        <img src="${image}" class="imgRes">
+                    </span>
+                </div>
+                <a href="#0" class="paperTitle" style="display: flex; justify-content: center;" > ${title} </a> 
+                <div class="authors" style="display: flex; justify-content: center;"> ${authors} </div> 
+                <br>
+                ${conferences ? `<div class="rConferences">${conferences}</div>` : ""}
+                ${researchYr ? `<div class="researchY">${researchYr}</div>` : ""}
+                <br>
+                ${primer ? `<div class="primer">${primer}</div>` : ""}
+                <br>
+                ${segundo ? `<div class="segundo">${segundo}</div>` : ""}
+                <br>
+                ${tercer ? `<div class="tercer">${tercer}</div>` : ""}
+                <br>
+                ${cuarto ? `<div class="tercer">${cuarto}</div>` : ""}
+            </td>
+        </tr>`)
+);
   researchTable.innerHTML = output;
 };
 document.addEventListener("DOMContentLoaded", fillData);
